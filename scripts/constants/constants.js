cf00fee8444f383ddcc8dc9169ccfafe1559cddb
alexanderhardeland <alexanderhardeland@gmail.com>
2022-06-16 15:19:02 +0200

    Starting on a clean slate to make the code work and do the lessons

--- scripts/constants/constants.js
+++
@@ -1,3 +0,0 @@
-export const inputHtmlElement = document.querySelector('#insert-value-input');
-export const buttonHtmlElement = document.querySelector('.main__input-section__button');
-export const orderedListContainer = document.querySelector('.main__ordered-list');
--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-import checkValueOnInput from "./components/checkValueOnInput.js";
-import { buttonHtmlElement } from "./constants/constants.js";
-import { addClass } from "./components/addClass.js";
-
-buttonHtmlElement.addEventListener("click", checkValueOnInput);
