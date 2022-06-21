c8d82b2c1065564359edfdfceaa890ba4b7a7327
Alexander Hardeland <alexanderhardeland.gmail.com>
2022-06-21 15:32:55 +0200

    Deleted everything and starting over again

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
-import generateListFromInputValue from "./components/generateListFromInputValue.js";
-import { buttonHtmlElement } from "./constants/constants.js";
+//TODO Imports
+let users = [
+  { id: 1, name: "Emma", gender: "Female" },
+  { id: 2, name: "Oscar", gender: "Male" },
+  { id: 3, name: "Alexis", gender: "Female" },
+  { id: 4, name: "Adam", gender: "Male" },
+];
 
-buttonHtmlElement.addEventListener('click', generateListFromInputValue);
+//TODO get the code in lesson 2 right
+
 
