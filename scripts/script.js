238791b759ca1d519448ab8afb059f5eeff74e75
Alexander Hardeland <alexanderhardeland.gmail.com>
2022-06-21 15:37:47 +0200

    Deleted everything and started over again

--- scripts/script.js
+++ scripts/script.js
@@ -1,80 +1,9 @@
-import { inputHtmlElement, buttonHtmlElement, orderedListElement } from "./constants/constants.js";
+//TODO Imports
+let users = [
+  { id: 1, name: "Emma", gender: "Female" },
+  { id: 2, name: "Oscar", gender: "Male" },
+  { id: 3, name: "Alexis", gender: "Female" },
+  { id: 4, name: "Adam", gender: "Male" },
+];
 
-const lengthOfInputValue = 3;
-const listKey = "list";
-let listItemStorage = [];
-
-const generateListFromInput = function() {
-  const valueInInput = inputHtmlElement.value.trim();
-
-  if(valueInInput.length >= lengthOfInputValue) {
-    listItemStorage.push(valueInInput);
-    createList();
-    saveToLocalStorage(valueInInput);
-
-    inputHtmlElement.value = "";
-    inputHtmlElement.focus();
-  }
-};
-
-const createList = function() {
-  orderedListElement.innerHTML = "";
-
-  for(let i = 0; i < listItemStorage.length; i++) {
-    let listItem = listItemStorage[i];
-
-    orderedListElement.innerHTML += `
-      <li>
-        <span class="list__item">${listItem}</span>
-        <i class="fa-solid fa-trash" data-item="${listItem}"></i>
-      </li>
-    `;
-  }
-
-  const listItems = document.querySelectorAll('.list__item');
-  for(let i = 0; i < listItems.length; i++) {
-    const listItem = listItems[i];
-
-    listItem.addEventListener('click', addClass);
-  }
-
-  const trashBins = document.querySelectorAll('.fa-solid');
-  for(let i = 0; i < trashBins.length; i++) {
-    const trashBin = trashBins[i];
-
-    trashBin.addEventListener('click', removeItemInList);
-  }
-};
-
-const addClass = function() {
-  event.target.classList.toggle('complete');
-};
-
-const removeItemInList = function() {
-  const listItemToDelete = event.target.dataset.item;
-
-  const outputNewList = listItemStorage.filter(function(listItem) {
-    if(listItemToDelete !== listItem) {
-      return true;
-    }
-  });
-
-  listItemStorage = outputNewList;
-  createList();
-};
-
-const saveToLocalStorage = function(itemInLocalStorage) {
-  localStorage.setItem(listKey, JSON.stringify(itemInLocalStorage));
-};
-
-const getListItemsFromLocalStorage = function() {
-  const getItemInLocalStorage = localStorage.getItem(listKey);
-  
-  if(!getItemInLocalStorage) {
-    return [];
-  } else {
-    return JSON.parse(getItemInLocalStorage);
-  }
-};
-
-buttonHtmlElement.addEventListener('click', generateListFromInput);
+//TODO get the code in lesson 2 right
