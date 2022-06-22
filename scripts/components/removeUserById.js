a98fb873ce9c547b25ac1403cb60b39b1e33c6cb
Alexander Hardeland <alexanderhardeland.gmail.com>
2022-06-22 14:27:53 +0200

    Now the code are working and lesson done

---
+++ scripts/components/removeUserById.js
@@ -0,0 +1,14 @@
+/*Create a function that removes any id from the users list
+export const removeUserById = function(idNumber, userList) {
+  const userToRemove = function(user) {
+    if(user.id !== idNumber) {
+      return true;
+    }
+  }
+  const newUsersList = userList.filter(userToRemove);
+
+  return userList = newUsersList;
+};
+*/
+
+//Do the same function with a on one line using arrow syntax
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-//TODO Imports
+//import { removeUserById } from "./components/removeUserById.js";
 let users = [
   { id: 1, name: "Emma", gender: "Female" },
   { id: 2, name: "Oscar", gender: "Male" },
@@ -6,4 +6,22 @@ let users = [
   { id: 4, name: "Adam", gender: "Male" },
 ];
 
+//Do the same function with a on one line using arrow syntax
+const idNumberToRemove = 1;
 //TODO get the code in lesson 2 right
+const removeUserById = function(idNumberToRemove) {
+  /*
+  const userToRemove = function(user) {
+    if(user.id !== idNumberToRemove) {
+      return true;
+    }
+  };
+  */
+
+  const newUsersList = users.filter((user) => user.id !== idNumberToRemove);
+  users = newUsersList;
+}
+
+console.log(users);
+removeUserById(idNumberToRemove);
+console.log(users);
